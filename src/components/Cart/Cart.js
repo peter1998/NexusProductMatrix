2bf945d3d0196ccc61655a0f1c8772370b9668db
Peter Matov <pmatov@gmail.com>
2023-08-03 10:04:23 +0300

    [C] Updating Cart Design and functionality

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,32 +1,52 @@
 import React, { useContext } from "react";
-import { Card, Button } from "react-bootstrap";
+import { Container, Card, Button } from "react-bootstrap";
 import { CartContext } from "../../context/cartContext";
-import "./cart.css"; // Import new CSS file
+import StarRatings from "react-star-ratings";
+import "./cart.css";
 
 function Cart() {
   const [cart, setCart] = useContext(CartContext);
 
   const removeFromCart = (productToRemove) => {
-    setCart(cart.filter((product) => product.id !== productToRemove.id));
+    if (
+      window.confirm(
+        "Are you sure you want to remove this item from your cart?"
+      )
+    ) {
+      setCart(cart.filter((product) => product.id !== productToRemove.id));
+    }
   };
 
+  const totalPrice = cart
+    .reduce((total, product) => total + product.price, 0)
+    .toFixed(2);
+
   return (
-    <div className="cart">
-      <h2>My Cart</h2>
-      {cart.map((product) => (
-        <Card className="cart-card" key={product.id}>
-          <Card.Img variant="top" src={product.image} />
-          <Card.Body>
-            <Card.Title>{product.name}</Card.Title>
-            <Card.Text>{product.description}</Card.Text>
-            <Card.Text>Price: {product.price}</Card.Text>
-            <Button variant="danger" onClick={() => removeFromCart(product)}>
-              Remove
-            </Button>
-          </Card.Body>
-        </Card>
-      ))}
-    </div>
+    <Container>
+      <div className="cart">
+        <h2>My Cart</h2>
+        {cart.map((product) => (
+          <Card className="cart-card" key={product.id}>
+            <Card.Img variant="top" src={product.image} />
+            <Card.Body>
+              <Card.Title>{product.name}</Card.Title>
+              <Card.Text>{product.description}</Card.Text>
+              <Card.Text>Price: {product.price}</Card.Text>
+              <StarRatings
+                rating={product.rating}
+                starRatedColor="orange"
+                numberOfStars={5}
+                name="rating"
+              />
+              <Button variant="danger" onClick={() => removeFromCart(product)}>
+                Remove
+              </Button>
+            </Card.Body>
+          </Card>
+        ))}
+        <h3>Total Price: {totalPrice}</h3>
+      </div>
+    </Container>
   );
 }
 
